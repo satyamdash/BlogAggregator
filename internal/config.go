f3864a1eed8848e91ab194395e1eca1e7773ce7b
satyamdash <satyam.bn6@gmail.com>
2025-10-20 17:00:41 +0530

    login handler done

---
+++ internal/config.go
@@ -0,0 +1,71 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+const configFileName = ".gatorconfig.json"
+
+type Config struct {
+	DB_URL            string
+	Current_User_Name string
+}
+
+func (c *Config) SetUser(current_user_name string) {
+	c.Current_User_Name = current_user_name
+	Write(c)
+}
+func (c *Config) SetDBUrl(url string) {
+	c.DB_URL = url
+	Write(c)
+}
+
+func getConfigFilePath() (string, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	configPath := filepath.Join(homeDir, configFileName)
+
+	return configPath, nil
+}
+
+func Read() (*Config, error) {
+	var cfg Config
+	configPath, err := getConfigFilePath()
+	if err != nil {
+		return nil, err
+	}
+	data, err := os.ReadFile(configPath)
+	if err != nil {
+		return nil, err
+	}
+	if err := json.Unmarshal(data, &cfg); err != nil {
+		return nil, err
+	}
+
+	fmt.Println(cfg.DB_URL)
+	fmt.Println(cfg.Current_User_Name)
+
+	return &cfg, nil
+}
+
+func Write(cfg *Config) error {
+	configPath, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+	data, err := json.MarshalIndent(cfg, "", "  ")
+	if err != nil {
+		return err
+	}
+	if err := os.WriteFile(configPath, data, 0777); err != nil {
+		return err
+	}
+	fmt.Println("Config updated successfully at:", configPath)
+	return nil
+}
--- main.go
+++ main.go
@@ -1,7 +1,76 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	config "github.com/satyamdash/BlogAggregator/internal"
+)
+
+type state struct {
+	cfg *config.Config
+}
+
+type commands struct {
+	CommandHandlerStore map[string]func(*state, command) error
+}
+
+func (c *commands) run(s *state, cmd command) error {
+
+	val, ok := c.CommandHandlerStore[cmd.name]
+	if !ok {
+		return fmt.Errorf("command does not exist")
+	}
+	return val(s, cmd)
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.CommandHandlerStore[name] = f
+}
+
+type command struct {
+	name     string
+	argslice []string
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.argslice) < 2 {
+		return fmt.Errorf("not enough arguments were provided")
+	}
+
+	if len(cmd.argslice) < 3 {
+		return fmt.Errorf("a username is required")
+	}
+
+	s.cfg.SetUser(cmd.argslice[2])
+	fmt.Printf("the user %s has been set", s.cfg.Current_User_Name)
+	return nil
+}
 
 func main() {
-	fmt.Println("Hello World")
+	cfg, err := config.Read()
+	if err != nil {
+		fmt.Print(err)
+	}
+	st := &state{cfg: cfg}
+	cmds := &commands{CommandHandlerStore: make(map[string]func(*state, command) error)}
+	cmds.register("login", handlerLogin)
+	args := os.Args
+	fmt.Printf("size of args %d", len(args))
+	if len(args) < 2 {
+		fmt.Println("usage: go run . <command> [args...]")
+		os.Exit(1)
+	}
+	// for idx, arg := range args {
+	// 	fmt.Printf("%d, %s", idx, arg)
+	// }
+	cmdName := args[1]
+	cmd := command{
+		name:     cmdName,
+		argslice: args,
+	}
+	if err := cmds.run(st, cmd); err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
 }
